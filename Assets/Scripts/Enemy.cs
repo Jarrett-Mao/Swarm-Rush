e9874b1b106328fb3b04529528bd61a68d67b231
Jarrett-Mao <74739111+Jarrett-Mao@users.noreply.github.com>
2022-05-01 17:12:40 -0700

    enemy can be destroyed on click

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health;
+    public int speed;
+    public GameObject gameObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown(){
+        health -= 1;
+        if (health == 0){
+            GameObject.Destroy(gameObject);
+        }
+        
+    }
+}
