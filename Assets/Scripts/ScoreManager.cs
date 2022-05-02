f7890920e11bb6a5310c0118b84cacd9ff4400aa
Jarrett-Mao <74739111+Jarrett-Mao@users.noreply.github.com>
2022-05-01 18:36:13 -0700

    score will display but does not count

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ScoreManager : MonoBehaviour
+{
+    
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI highscoreText;
+
+    int score = 0;
+    int highscore = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.text = "Score: " + score.ToString();
+        highscoreText.text = "HIGHSCORE: " + highscore.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
