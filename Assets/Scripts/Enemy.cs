e6d9dc7b388166488e018bfc301f573d795936ee
Jarrett-Mao <74739111+Jarrett-Mao@users.noreply.github.com>
2022-05-02 20:34:56 -0700

    zergling will move towards the tesseract but the rotation is messed up and does not work with spawning

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,19 +5,38 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public int health;
-    public int speed;
+    public float speed;
     public GameObject gameObject;
     public ScoreManager scoreManager;
+
+    public Transform tesseract;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Vector3 direction = tesseract.position - transform.position;
+        // Debug.Log(tesseract.position);
+        rb = this.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var direction = tesseract.position - transform.position;
+        var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        direction.Normalize();
+        movement = direction;
+    }
+    private void FixedUpdate() {
+        moveCharacter(movement);
+    }
+
+    void moveCharacter(Vector2 direction){
+        rb.MovePosition((Vector2)transform.position + (direction * speed * Time.deltaTime));
     }
 
     void OnMouseDown(){
