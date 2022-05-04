a204cd19bd26ebcb5e5c5c621671297d64e3bdbc
Jarrett-Mao <74739111+Jarrett-Mao@users.noreply.github.com>
2022-05-03 20:36:40 -0700

    wall script is setup but imperfect

---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Wall : MonoBehaviour
+{
+    public float maxHealth;
+    public float health;
+    public Slider slider;
+    public GameObject healthBarUI;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+        slider.value = calculateHealth();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        slider.value = calculateHealth(); //health bar status
+        if(health < maxHealth){
+            healthBarUI.SetActive(true); 
+        } 
+        if (health <= 0){
+            destroySelf();
+        }
+
+        // checkCollision();
+        
+    }
+
+        void destroySelf(){
+        GameObject.Destroy(gameObject);
+    }
+
+    float calculateHealth(){
+        return health / maxHealth;
+    }
+
+    private void OnTriggerEnter2D(Collider2D enemy){
+        Debug.Log("under attack");
+    }
+}
